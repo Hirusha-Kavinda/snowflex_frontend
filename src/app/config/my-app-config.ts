b93347ed495bc6cf7fde623e7bc5b6f3a2b416a0
Hirusha-Kavinda <138157078+Hirusha-Kavinda@users.noreply.github.com>
2023-09-28 22:04:53 +0530

    okta auth call

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc:{
+        clientId : '0oabm1ehdz1cY0Oue5d7',
+        issure: 'https://dev-94530975.okta.com/oauth2/default',
+        redirectUrl : 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+}
